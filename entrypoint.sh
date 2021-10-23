511093c6c92420aea7f97e98eb99430173c422db
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-10-23 16:52:46 +0800

    Add entrypoint script for config generation

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ -f "/usr/src/api/config/settings.json"]] && [[ -f "/usr/src/api/config/template.json" ]]; then
+  echo "warning: You have both the template and custom settings JSOn files! Please comment out line 10"
+  echo "warning: of your Dockerfile, as the entrypoint script will not overwrite them!"
+elif [[ ! -f "/usr/src/api/config/settings.json"]] && [[ -f "/usr/src/api/config/template.json" ]]; then
+  envsubst < /usr/src/api/config/template.json > /usr/src/api/config/settings.json
+fi
+sleep 3
+
+# then exec to the dist binary
+exec node dist/api.js
